78c6f9058e29f13564df86f73f4e814ba6482c18
Julian Martin <juliantatemartin@icloud.com>
2024-11-23 19:38:40 +0000

    v1.4

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer 
-            className=" text-white text-center py-3"
-            style={{ backgroundColor: 'rgba(42, 38, 56, 1)' }}>
+    <footer className="text-white text-center py-3" style={{ backgroundColor: '#212121' }}>
       <div className="container">
         <p className="mb-0">&copy; 2024 Dyscord Bots. All rights reserved.</p>
       </div>
